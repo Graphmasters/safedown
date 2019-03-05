59b0e219a2ee1d9f1ed8be2d4e99cc21862feaf3
pfinch <peter.finch@graphmasters.net>
2019-03-05 10:54:21 +0100

    :sparkles: Introducing onSignal method.

--- safedown.go
+++ safedown.go
@@ -15,22 +15,26 @@ const (
 
 // ShutdownActions is a set of actions that are run when the os receives an Interrupt signal.
 type ShutdownActions struct {
-	order        Order
-	actions      []func()
-	closeCh      chan struct{}
-	closeOnce    sync.Once
-	shutdownOnce sync.Once
+	order         Order
+	actions       []func()
+	onSignalMutex sync.Mutex
+	onSignalFunc  func(os.Signal)
+	closeCh       chan struct{}
+	closeOnce     sync.Once
+	shutdownOnce  sync.Once
 }
 
 // NewShutdownActions creates a new set of shutdown actions and starts listen for any of the signals provided.
 // The order determines the order the actions will be executed.
 func NewShutdownActions(order Order, signals ...os.Signal) *ShutdownActions {
 	sa := &ShutdownActions{
-		order:        order,
-		actions:      make([]func(), 0),
-		closeCh:      make(chan struct{}),
-		closeOnce:    sync.Once{},
-		shutdownOnce: sync.Once{},
+		order:         order,
+		actions:       make([]func(), 0),
+		onSignalMutex: sync.Mutex{},
+		onSignalFunc:  nil,
+		closeCh:       make(chan struct{}),
+		closeOnce:     sync.Once{},
+		shutdownOnce:  sync.Once{},
 	}
 
 	go sa.start(signals)
@@ -42,12 +46,29 @@ func (sa *ShutdownActions) AddActions(actions ...func()) {
 	sa.actions = append(sa.actions, actions...)
 }
 
+// SetOnSignal sets the method which will be called when a signal is received
+func (sa *ShutdownActions) SetOnSignal(onSignal func(os.Signal)) {
+	sa.onSignalMutex.Lock()
+	defer sa.onSignalMutex.Unlock()
+	sa.onSignalFunc = onSignal
+}
+
 // Shutdown sends close message and shuts down
 func (sa *ShutdownActions) Shutdown() {
 	sa.closeOnce.Do(func() {
 		close(sa.closeCh)
 	})
 	sa.shutdown()
+}
+
+// onSignal passes the signal calls the on signal function
+func (sa *ShutdownActions) onSignal(s os.Signal) {
+	sa.onSignalMutex.Lock()
+	defer sa.onSignalMutex.Unlock()
+
+	if sa.onSignalFunc != nil {
+		sa.onSignalFunc(s)
+	}
 }
 
 // start listens for an interrupt signal and runs
@@ -58,7 +79,8 @@ func (sa *ShutdownActions) start(signals []os.Signal) {
 
 	// Listens for signals and close message
 	select {
-	case <-signalCh:
+	case s := <-signalCh:
+		sa.onSignal(s)
 	case <-sa.closeCh:
 	}
 
--- safedown_test.go
+++ safedown_test.go
@@ -2,12 +2,16 @@ package safedown
 
 import (
 	"fmt"
+	"os"
 	"syscall"
 )
 
 func ExampleNewShutdownActions() {
 	sa := NewShutdownActions(FirstInLastDone, syscall.SIGTERM, syscall.SIGINT)
 	defer sa.Shutdown()
+	sa.SetOnSignal(func(signal os.Signal) {
+		fmt.Printf("A signal was received: %s\n", signal.String())
+	})
 
 	sa.AddActions(func() {
 		fmt.Println("... and this will be done last.")
