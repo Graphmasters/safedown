b29c76fa6638febe30d8f0d9a052a7600166b4e5
Peter Finch <peter.finch@graphmasters.net>
2020-01-24 11:03:38 +0100

    :art: Improving code (#6)

--- safedown.go
+++ safedown.go
@@ -7,6 +7,7 @@ import (
 	"sync"
 )
 
+// Order represents the order that the shutdown actions will be executed.
 type Order bool
 
 const (
@@ -14,95 +15,118 @@ const (
 	FirstInLastDone  Order = false // Actions are executed in the reversed order they are added.
 )
 
-// ShutdownActions is a set of actions that are run when the os receives an Interrupt signal.
+// ShutdownActions contains actions that are run when the os receives an interrupt signal.
+// This object must be created using the NewShutdownActions function.
 type ShutdownActions struct {
-	order         Order           // This determines the order the actions will be done.
-	actions       []func()        // The actions done on shutdown.
-	onSignalMutex sync.Mutex      // A mutex for handling the onSignalFunc.
-	onSignalFunc  func(os.Signal) // The function to be called when a signal is received.
-	stopCh        chan struct{}   // A channel to stop listening for signals.
-	stopOnce      sync.Once       // Ensures listening to signals is stopped once.
-	shutdownOnce  sync.Once       // Ensures shutdown actions are only done once.
+	order        Order           // This determines the order the actions will be done.
+	actions      []func()        // The actions done on shutdown.
+	onSignalFunc func(os.Signal) // The function to be called when a signal is received.
+	stopCh       chan struct{}   // A channel to stop listening for signals.
+	stopOnce     sync.Once       // Ensures listening to signals is stopped once.
+	shutdownOnce sync.Once       // Ensures shutdown actions are only done once.
+	mutex        sync.Mutex      // A mutex to avoid clashes handling actions or onSignal.
 }
 
-// NewShutdownActions creates a new set of shutdown actions and starts listen for any of the signals provided.
+// NewShutdownActions creates and initialises a new set of shutdown actions.
+// The actions (added later) will be executed if any of the signals provided are received.
 // The order determines the order the actions will be executed.
 func NewShutdownActions(order Order, signals ...os.Signal) *ShutdownActions {
+	// Creates struct with order and stop channel
 	sa := &ShutdownActions{
-		order:         order,
-		actions:       make([]func(), 0),
-		onSignalMutex: sync.Mutex{},
-		onSignalFunc:  nil,
-		stopCh:        make(chan struct{}),
-		stopOnce:      sync.Once{},
-		shutdownOnce:  sync.Once{},
+		order:  order,
+		stopCh: make(chan struct{}),
 	}
 
-	go sa.start(signals)
+	// If there are no signals to listen to then the stop channel is not required and initialisation is complete
+	if len(signals) == 0 {
+		sa.closeStopCh()
+		return sa
+	}
+
+	// Creates channel to receive notification signals
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, signals...)
+
+	// Starts a go routine for listening for signals and close messages
+	go func() {
+		// Listens for signal or close message
+		var received os.Signal
+		select {
+		case <-sa.stopCh:
+		case received = <-signalCh:
+		}
+
+		// Stops listening for signals and closes channels
+		signal.Stop(signalCh)
+		close(signalCh)
+		sa.closeStopCh()
+
+		// Runs on signal and shutdown actions
+		sa.onSignal(received)
+		sa.shutdown()
+	}()
+
 	return sa
 }
 
 // AddActions adds actions to be run on shutdown or when a signal is received.
+// Any action added after a signal has been received or the Shutdown method has been called will not be executed.
 func (sa *ShutdownActions) AddActions(actions ...func()) {
+	sa.mutex.Lock()
 	sa.actions = append(sa.actions, actions...)
+	sa.mutex.Unlock()
 }
 
-// SetOnSignal sets the method which will be called when a signal is received.
+// SetOnSignal sets the method which will be called if a signal is received.
 func (sa *ShutdownActions) SetOnSignal(onSignal func(os.Signal)) {
-	sa.onSignalMutex.Lock()
-	defer sa.onSignalMutex.Unlock()
+	sa.mutex.Lock()
 	sa.onSignalFunc = onSignal
+	sa.mutex.Unlock()
 }
 
-// Shutdown runs the shutdown actions and stops listening for signals.
+// Shutdown runs the shutdown actions and stops listening for signals (if doing so).
 func (sa *ShutdownActions) Shutdown() {
+	sa.closeStopCh()
+	sa.shutdown()
+}
+
+// closeStopCh closes the stop channel
+func (sa *ShutdownActions) closeStopCh() {
 	sa.stopOnce.Do(func() {
 		close(sa.stopCh)
 	})
-	sa.shutdown()
 }
 
 // onSignal passes the signal received to the on signal function
 func (sa *ShutdownActions) onSignal(s os.Signal) {
-	sa.onSignalMutex.Lock()
-	defer sa.onSignalMutex.Unlock()
-
-	if sa.onSignalFunc != nil {
-		sa.onSignalFunc(s)
-	}
-}
-
-// start listens for an interrupt signal and runs
-func (sa *ShutdownActions) start(signals []os.Signal) {
-	// Checks if there are signals to listen for
-	if len(signals) == 0 {
+	// Ensures signal is not nil
+	if s == nil {
 		return
 	}
 
-	// Notification channel
-	signalCh := make(chan os.Signal, 1)
-	signal.Notify(signalCh, signals...)
-
-	// Listens for signals and close message
-	select {
-	case s := <-signalCh:
-		sa.onSignal(s)
-	case <-sa.stopCh:
+	// Gets the on signal function and checks that it is not nil
+	var onSignal func(os.Signal)
+	sa.mutex.Lock()
+	onSignal = sa.onSignalFunc
+	sa.mutex.Unlock()
+	if onSignal == nil {
+		return
 	}
 
-	// Stops listening for signals
-	signal.Stop(signalCh)
-	close(signalCh)
-
-	// runs shutdown actions
-	sa.shutdown()
+	// Calls the on signal function
+	onSignal(s)
 }
 
 // shutdown runs the shutdown actions
 func (sa *ShutdownActions) shutdown() {
 	sa.shutdownOnce.Do(
 		func() {
+			// Gets current length of actions
+			sa.mutex.Lock()
 			l := len(sa.actions)
+			sa.mutex.Unlock()
+
+			// Executes actions in order
 			for i := 0; i < l; i++ {
 				if sa.order == FirstInFirstDone {
 					sa.actions[i]()
--- safedown_test.go
+++ safedown_test.go
@@ -3,20 +3,30 @@ package safedown
 import (
 	"fmt"
 	"os"
+	"sync"
 	"syscall"
+	"testing"
+	"time"
 )
 
+// region Examples
+
 func ExampleNewShutdownActions() {
+	// Creates the shutdown actions and defers the Shutdown method.
 	sa := NewShutdownActions(FirstInLastDone, syscall.SIGTERM, syscall.SIGINT)
 	defer sa.Shutdown()
+
+	// Sets the function to be called if a signal is received
 	sa.SetOnSignal(func(signal os.Signal) {
 		fmt.Printf("A signal was received: %s\n", signal.String())
 	})
 
+	// The first action added will be the last done
 	sa.AddActions(func() {
 		fmt.Println("... and this will be done last.")
 	})
 
+	// The last action added will be the first done
 	sa.AddActions(func() {
 		fmt.Println("This will be done first ...")
 	})
@@ -25,3 +35,121 @@ func ExampleNewShutdownActions() {
 	// This will be done first ...
 	// ... and this will be done last.
 }
+
+// endregion
+
+// region Tests
+
+// TestShutdownActions_OnSignal tests if the correct signal is received.
+// It also indirectly checks that the shutdown actions are intercepting
+// signals as the test will simply fail otherwise.
+// nolint: gomnd
+func TestShutdownActions_OnSignal(t *testing.T) {
+	finished := setTestTimeout(t, 3*time.Second)
+	defer finished()
+
+	wg := sync.WaitGroup{}
+	sa := NewShutdownActions(FirstInLastDone, os.Interrupt)
+	wg.Add(1)
+	sa.SetOnSignal(func(signal os.Signal) {
+		if signal != os.Interrupt {
+			t.Logf("signal received was %s instead of %s", signal.String(), os.Interrupt.String())
+			t.Fail()
+		}
+		wg.Done()
+	})
+
+	sendSignalToSelf(t, os.Interrupt)
+	wg.Wait()
+}
+
+// TestNewShutdownActions_FirstInFirstDone checks that
+// actions are down in the order they were added.
+// nolint: gomnd
+func TestNewShutdownActions_FirstInFirstDone(t *testing.T) {
+	finished := setTestTimeout(t, time.Second)
+	defer finished()
+
+	count := 0
+	wg := sync.WaitGroup{}
+	sa := NewShutdownActions(FirstInFirstDone)
+	sa.AddActions(counter(t, &wg, 1, &count))
+	sa.AddActions(counter(t, &wg, 2, &count))
+	sa.AddActions(counter(t, &wg, 3, &count))
+	sa.Shutdown()
+	wg.Wait()
+}
+
+// TestNewShutdownActions_FirstInLastDone checks that
+// actions are down in the opposite order to the order they
+// were added.
+// nolint: gomnd
+func TestNewShutdownActions_FirstInLastDone(t *testing.T) {
+	cancel := setTestTimeout(t, time.Second)
+	defer cancel()
+
+	count := 0
+	wg := sync.WaitGroup{}
+	sa := NewShutdownActions(FirstInLastDone)
+	sa.AddActions(counter(t, &wg, 3, &count))
+	sa.AddActions(counter(t, &wg, 2, &count))
+	sa.AddActions(counter(t, &wg, 1, &count))
+	sa.Shutdown()
+	wg.Wait()
+}
+
+// endregion
+
+// region Helpers
+
+// counter creates a function that should be added to the shutdown actions.
+// The test will fail if the value given doesn't increment to the expected value.
+// nolint: gomnd
+func counter(t *testing.T, wg *sync.WaitGroup, expected int, value *int) func() {
+	wg.Add(1)
+	return func() {
+		*value++
+		if *value != expected {
+			t.Fail()
+		}
+		wg.Done()
+	}
+}
+
+// sendSignalToSelf sends the signal provided to the current process.
+func sendSignalToSelf(t *testing.T, signal os.Signal) {
+	pid := os.Getpid()
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		t.Log("could not find process")
+		t.Fail()
+		return
+	}
+
+	err = process.Signal(signal)
+	if err != nil {
+		t.Log("could not send signal to process")
+		t.Fail()
+	}
+}
+
+// setTestTimeout returns a function which should be called at the end of the test.
+// If the function is not called before the timeout the test fails.
+func setTestTimeout(t *testing.T, timeout time.Duration) func() {
+	closeCh := make(chan struct{})
+
+	go func() {
+		select {
+		case <-closeCh:
+		case <-time.After(timeout):
+			t.Log("test exceeded timeout")
+			t.Fail()
+		}
+	}()
+
+	return func() {
+		close(closeCh)
+	}
+}
+
+// endregion
